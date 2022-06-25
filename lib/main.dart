bccd749ef291984e907c9abd3ca35dd2345b1239
asahaya <asa7kadensony@gmail.com>
2022-06-25 09:59:53 +0900

    aaa

--- lib/main.dart
+++ lib/main.dart
@@ -48,7 +48,7 @@ class _PixabayPageState extends State<PixabayPage> {
           itemCount: imageList.length,
           itemBuilder: (context, index) {
             Map<String, dynamic> image = imageList[index];
-            //return
+
             return Image.network(image['previewURL']);
           }),
     );
