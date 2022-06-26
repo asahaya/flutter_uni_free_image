4aceeca7807b7e56a00d3a705060cac8f6335410
asahaya <asa7kadensony@gmail.com>
2022-06-26 12:16:45 +0900

    kouhan

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:share_plus/share_plus.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,9 +29,9 @@ class PixabayPage extends StatefulWidget {
 class _PixabayPageState extends State<PixabayPage> {
   List imageList = [];
 
-  Future<void> feachImage() async {
+  Future<void> feachImage(String textSearch) async {
     Response response = await Dio().get(
-        "https://pixabay.com/api/?key=24474456-12b32c094cd3754e35ab5af34&q=yellow+flowers&image_type=photo");
+        "https://pixabay.com/api/?key=24474456-12b32c094cd3754e35ab5af34&q=$textSearch&image_type=photo&per_page=100");
     imageList = response.data['hits'];
     print(imageList);
     setState(() {});
@@ -36,20 +40,70 @@ class _PixabayPageState extends State<PixabayPage> {
   @override
   void initState() {
     super.initState();
-    feachImage();
+    feachImage('tv');
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: TextFormField(
+          decoration: InputDecoration(
+            fillColor: Colors.white,
+            filled: true,
+          ),
+          onFieldSubmitted: (taptext) {
+            print(taptext);
+            feachImage(taptext);
+          },
+        ),
+      ),
       body: GridView.builder(
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 3),
           itemCount: imageList.length,
           itemBuilder: (context, index) {
             Map<String, dynamic> image = imageList[index];
-            Map<String, dynamic> a = imageList[index];
-            return Image.network(image['previewURL']);
+
+            return InkWell(
+              onTap: () async {
+                Directory dir = await getTemporaryDirectory();
+
+                Response res = await Dio().get(image['webformatURL'],
+                    options: Options(
+                      responseType: ResponseType.bytes,
+                    ));
+                File imageFile =
+                    await File("${dir.path}/image.png").writeAsBytes(res.data);
+
+                await Share.shareFiles([imageFile.path]);
+              },
+              child: Stack(
+                fit: StackFit.expand,
+                children: [
+                  Image.network(
+                    image['previewURL'],
+                    fit: BoxFit.cover,
+                  ),
+                  Align(
+                    alignment: Alignment.bottomRight,
+                    child: Container(
+                      color: Colors.white.withOpacity(0.3),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          const Icon(
+                            Icons.thumb_up_alt_outlined,
+                          ),
+                          Text(image['likes'].toString()),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
           }),
     );
   }
